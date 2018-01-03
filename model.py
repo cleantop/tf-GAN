f9bda576067ecbe3c915e1098d213bb824d259da
yeachankim <yeachan@korea.ac.kr>
2018-01-03 23:39:56 +0900

    Initial commit

---
+++ model.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+
+
+class SimpleGAN(object):
+
+    def __init__(self):
+
+        # Generator Variables
+        self.G_w1 = tf.get_variable(name="G_W1", shape=[100, 128], initializer=tf.contrib.layers.xavier_initializer())
+        self.G_b1 = tf.Variable(tf.constant(0.0, shape=[128], name="G_b1"))
+        self.G_w2 = tf.get_variable(name="G_W2", shape=[128, 784], initializer=tf.contrib.layers.xavier_initializer())
+        self.G_b2 = tf.Variable(tf.constant(0.0, shape=[784], name="G_b2"))
+
+        self.G_vars = [self.G_w1, self.G_b1, self.G_w2, self.G_b2]
+
+        # Discriminator Variables
+        self.D_w1 = tf.get_variable(name="D_W1", shape=[784, 128], initializer=tf.contrib.layers.xavier_initializer())
+        self.D_b1 = tf.Variable(tf.constant(0.0, shape=[128], name="D_b1"))
+        self.D_w2 = tf.get_variable(name="D_W2", shape=[128, 1], initializer=tf.contrib.layers.xavier_initializer())
+        self.D_b2 = tf.Variable(tf.constant(0.0, shape=[1], name="D_b2"))
+
+        self.D_vars = [self.D_w1, self.D_b1, self.D_w2, self.D_b2]
+
+
+        # Random noise setting for Generator
+        self.z = tf.placeholder(tf.float32, shape=[None, 100])
+
+        # Real image
+        self.x = tf.placeholder(tf.float32, shape=[None, 784])
+
+        self.G_sample = self.generator(self.z)
+
+        D_real, D_logit_real = self.discriminator(self.x)
+        D_fake, D_logit_fake = self.discriminator(self.G_sample)
+
+        # Loss functions
+        self.D_loss = -tf.reduce_mean(tf.log(D_real) + tf.log(1.0 - D_fake))
+        self.G_loss = -tf.reduce_mean(tf.log(D_fake))
+
+    # Generate Fake image(28*28) from noise distribution
+    def generator(self, z):
+        # Noise to hidden
+        G_h1 = tf.nn.relu(tf.matmul(z, self.G_w1) + self.G_b1)
+
+        # Hidden to fake image
+        G_logit = tf.matmul(G_h1, self.G_w2) + self.G_b2
+        G_prob = tf.nn.sigmoid(G_logit)
+
+        return G_prob
+
+    # Determine that input is real or fake image
+    def discriminator(self, x):
+
+        # Image to hidden
+        D_h1 = tf.nn.relu(tf.matmul(x, self.D_w1) + self.D_b1)
+
+        # Hidden to fake image
+        D_logit = tf.matmul(D_h1, self.D_w2) + self.D_b2
+        D_prob = tf.nn.sigmoid(D_logit)
+
+        return D_prob, D_logit
+
+
---
+++ train.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from model import SimpleGAN
+from tensorflow.examples.tutorials.mnist import input_data
+
+# Hyper-parameters
+epoch = 1000000
+batch_size = 100
+sample_size = 10
+
+# Load MNIST Dataset
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+config = tf.ConfigProto(allow_soft_placement=True)
+with tf.Session(config=config) as sess:
+    with sess.as_default():
+
+        # GAN object
+        gan = SimpleGAN()
+
+
+        # Define Training procedures of Generator
+        G_global_step = tf.Variable(0, name="G_global_step", trainable=True)
+
+        G_optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        G_grads_and_vars = G_optimizer.compute_gradients(gan.G_loss, var_list=gan.G_vars)
+        G_train_op = G_optimizer.apply_gradients(G_grads_and_vars, global_step=G_global_step)
+
+        # Define Training procedures of Discriminator
+        D_global_step = tf.Variable(0, name="D_global_step", trainable=True)
+        D_optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        D_grads_and_vars = D_optimizer.compute_gradients(gan.D_loss, var_list=gan.D_vars)
+        D_train_op = D_optimizer.apply_gradients(D_grads_and_vars, global_step=D_global_step)
+
+        # Initialize all variables
+        sess.run(tf.global_variables_initializer())
+
+        def generate_noise_sample(m, n):
+            return np.random.uniform(-1, 1, size=[m, n])
+
+        # Training step
+        for i in range(epoch):
+            x, _ = mnist.train.next_batch(batch_size)
+
+            # Discriminator training
+            _, D_step, D_loss = sess.run([D_train_op, D_global_step, gan.D_loss],
+                                          feed_dict={gan.x: x, gan.z: generate_noise_sample(batch_size, 100)})
+
+            # Generator training
+            _, G_step, G_loss = sess.run([G_train_op, G_global_step, gan.G_loss],
+                                          feed_dict={gan.z: generate_noise_sample(batch_size, 100)})
+
+            # Validating image
+            if i % 50000 == 0:
+                print(i)
+
+                noise = generate_noise_sample(sample_size**2, 100)
+                samples = sess.run(gan.G_sample, feed_dict={gan.z: noise})
+                fig = plt.figure()
+                for s in range(sample_size**2):
+                    ax = fig.add_subplot(sample_size, sample_size, s + 1)
+                    ax.set_axis_off()
+                    ax.imshow(np.reshape(samples[s], (28, 28)))
+                plt.savefig('samples/{}.png'.format(str(i)))
+                plt.close(fig)
